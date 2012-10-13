064ccb45f01566ce297b0776b07ca88ddc0a104a
Marko Burjek <s0mebody.slo@gmail.com>
2012-10-13 21:02:01 +0200

    Added sqlalchemy dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from distutils.core import setup
 setup(
         name='TogglViz',
         version='0.1dev',
+        author='Marko Burjek',
         packages=['togglviz', ],
-        license='GPL 2',
+        license='LICENSE.txt',
         long_description=open('README.txt').read(),
+        install_requires=[
+            "SQLAlchemy==0.7.9",
+        ],
+
 )
