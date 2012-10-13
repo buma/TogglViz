a0d710cff2ade01718413cedab01afe83ed60cb5
Marko Burjek <s0mebody.slo@gmail.com>
2012-10-13 20:16:09 +0200

    First commit of TogglViz

---
+++ README.txt
@@ -0,0 +1,5 @@
+===========
+Toggl Viz
+===========
+
+Toggl Viz is a vizualizer for Toggl data which I use for my time tracking
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name='TogglViz',
+        version='0.1dev',
+        packages=['togglviz', ],
+        license='GPL 2',
+        long_description=open('README.txt').read(),
+)
